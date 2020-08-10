0679dacd366fb2c40d82db9aafb51f644e4fb878
Gobeli <etienne.gobeli@students.fhnw.ch>
2020-08-10 19:51:22 +0200

    optimize admin script

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -6,13 +6,8 @@ const html = htm.bind(h);
 
 var HomePreview = createClass({
   render: function () {
-    var entry = this.props.entry;
-    var image = entry.getIn(['data', 'photo']);
-    var photo = this.props.getAsset(image);
-    var title = entry.getIn(['data', 'title']);
-    var intro = entry.getIn(['data', 'intro']);
     const index = new Index();
-    return html([index.render({ home: { photo, title, intro } })]);
+    return html([index.render({ home: this.props.entry.get('data').toJS() })]);
   },
 });
 
