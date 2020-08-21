092ab78004fce621ca47b343abefe6903dfa720c
Gobeli <etienne.gobeli@students.fhnw.ch>
2020-08-21 10:33:00 +0200

    enable responsive images

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const pluginLocalRespimg = require('eleventy-plugin-local-respimg');
 const markdownFilter = require('./src/filters/markdown-filter.js');
 
 module.exports = function (config) {
@@ -7,6 +8,25 @@ module.exports = function (config) {
   config.addPassthroughCopy('static');
   config.addPassthroughCopy('src/content/admin/config.yml');
   config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
+
+  config.addPlugin(pluginLocalRespimg, {
+    folders: {
+      source: '.', 
+      output: 'dist', 
+    },
+    images: {
+      resize: {
+        min: 250, 
+        max: 1500,
+        step: 150,
+      },
+      sizes: '100vw',
+      lazy: true,
+      watch: {
+        src: 'static/images/**/*', // Glob of images that Eleventy should watch for changes to
+      }
+    }
+  });
 
   config.addWatchTarget('src/scripts/**/*.js');
 
