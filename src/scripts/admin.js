8a7005f720c3778992ab9cd752d38acbbfa3664c
gobeli <etienne.gobeli@students.fhnw.ch>
2020-09-03 14:41:19 +0200

    add layouts

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -4,21 +4,21 @@ const env = nunjucks.configure();
 
 env.addFilter('markdown', markdownFilter);
 
-const removeFrontMatter = (html) => {
-  const template = html.split('---');
-  return template.length > 2 ? template[2] : template[0];
-};
-
 const Preview = ({ entry, path, context }) => {
-  const data = context(entry.get('data').toJS() );
+  const data = context(entry.get('data').toJS());
   data.content = data.body;
   const html = env.render(path, data);
 
-  return <div dangerouslySetInnerHTML={{ __html: removeFrontMatter(html) }} />;
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 const Home = ({ entry }) => (
   <Preview entry={entry} path="layouts/home.njk" context={(context) => context} />
 );
 
+const Post = ({ entry }) => (
+  <Preview entry={entry} path="layouts/post.njk" context={(context) => context} />
+);
+
 CMS.registerPreviewTemplate('home', Home);
+CMS.registerPreviewTemplate('blog', Post);
